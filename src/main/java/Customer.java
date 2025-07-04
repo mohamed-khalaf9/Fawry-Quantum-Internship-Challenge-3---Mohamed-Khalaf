973259189d35ba4007d874b10ec80ec795dcf8fd
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-04 22:32:33 +0300

    define customer constructor, validate the accepted balance and name

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -2,5 +2,14 @@ public class Customer {
     private String name;
     private double currentBalance;
 
+    public Customer(double currentBalance, String name) {
+        if(currentBalance < 0)
+            throw new IllegalArgumentException("Invalid balance");
+        if(name == null || name.isEmpty())
+            throw new IllegalArgumentException("Invalid name");
+
+        this.currentBalance = currentBalance;
+        this.name = name;
+    }
 }
 
