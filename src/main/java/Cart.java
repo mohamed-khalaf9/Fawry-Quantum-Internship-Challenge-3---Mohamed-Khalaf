396cca69001b09ca85c15d9186bb442b6689eff8
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-05 01:11:22 +0300

    implement the logic of add method

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,5 +7,24 @@ public class Cart {
 
     public Cart() {
         items = new ArrayList<CartItem>();
+    }
+
+    public void add(Product product, int quantity) {
+        if(quantity <= 0) {
+            throw new IllegalArgumentException("Quantity must be greater than 0");
+        }
+        if(product == null) {
+            throw new IllegalArgumentException("Product cannot be null");
+        }
+        if(product instanceof Expirable) {
+            if(((Expirable) product).getExpiryDate().isBefore(LocalDate.now()))
+                throw new IllegalArgumentException("The Product is expired");
+        }
+        if(quantity > product.getQuantity()) {
+            throw new IllegalArgumentException("The stock not enough");
+        }
+        CartItem item = new CartItem(product, quantity);
+        items.add(item);
+
     }
 }
