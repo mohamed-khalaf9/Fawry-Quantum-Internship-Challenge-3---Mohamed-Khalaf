2caec689e6fdc12083b1e8d93854d8f33f5f1111
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-04 23:00:59 +0300

    - add expiry Date data member becuase cheese is expirable - define implementation for getWeight and get expiryDate()

--- src/main/java/Cheese.java
+++ src/main/java/Cheese.java
@@ -1,12 +1,28 @@
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Cheese extends  Product implements Shippable, ShippableItem,Expirable {
     private double weight;
+    private LocalDate expiryDate;
 
-    public Cheese(String name, int qunatity, double price, double weight) {
+    public Cheese(String name, int qunatity, double price, double weight,LocalDate expiryDate) {
         super(name, qunatity, price);
         if(weight < 0)
             throw new IllegalArgumentException("Weight cannot be negative");
+        if(expiryDate == null)
+            throw new IllegalArgumentException("Expiry date cannot be null");
+
+        this.expiryDate = expiryDate;
         this.weight = weight;
     }
 
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
 
+    @Override
+    public LocalDate getExpiryDate() {
+        return this.expiryDate;
+    }
 }
