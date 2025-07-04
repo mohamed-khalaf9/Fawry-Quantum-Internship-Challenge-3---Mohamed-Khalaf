924984f18ccede719459dcf9822ef285b038e888
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-04 23:15:22 +0300

    define biscuits product which is expirable product

---
+++ src/main/java/Biscuits.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+
+public class Biscuits extends  Product implements Expirable {
+    private LocalDate expiryDate;
+
+    public Biscuits(String name, int qunatity, double price,LocalDate expiryDate) {
+        super(name, qunatity, price);
+        if(expiryDate == null)
+            throw new IllegalArgumentException("Expiry date cannot be null");
+
+        this.expiryDate = expiryDate;
+
+    }
+
+
+
+    @Override
+    public LocalDate getExpiryDate() {
+        return this.expiryDate;
+    }
+
+
+}
+
