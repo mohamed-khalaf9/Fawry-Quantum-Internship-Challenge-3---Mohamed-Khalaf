79fcd98ca155ce9fb59ee04e6deaa0852d673867
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-04 23:08:55 +0300

    define tv product class which is shippable product

---
+++ src/main/java/TV.java
@@ -0,0 +1,25 @@
+
+public class TV extends  Product implements Shippable, ShippableItem {
+    private double weight;
+
+
+    public TV(String name, int qunatity, double price, double weight) {
+        super(name, qunatity, price);
+        if(weight < 0)
+            throw new IllegalArgumentException("Weight cannot be negative");
+
+        this.weight = weight;
+    }
+
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+
+
+    public String getName()
+    {
+        return super.getName();
+    }
+
+}
