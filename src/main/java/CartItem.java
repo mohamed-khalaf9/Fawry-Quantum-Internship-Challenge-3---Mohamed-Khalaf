8042d914627d0b43192be187a3cb668248a98cb1
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-04 22:37:38 +0300

    define constuctor with validation to initalize product and quantity

--- src/main/java/CartItem.java
+++ src/main/java/CartItem.java
@@ -1,4 +1,12 @@
 public class CartItem {
     private Product product;
     private int quantity;
+    public CartItem(Product product, int quantity) {
+        if(product == null)
+            throw new NullPointerException("product is null");
+        if(quantity < 0)
+            throw new IllegalArgumentException("quantity is negative");
+        this.product = product;
+        this.quantity = quantity;
+    }
 }
