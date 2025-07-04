e8608c32136e738b10e3385280a1969e894a15a5
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-04 22:50:54 +0300

    define cheese constructor

--- src/main/java/Cheese.java
+++ src/main/java/Cheese.java
@@ -1,5 +1,12 @@
 public class Cheese extends  Product implements Shippable, ShippableItem,Expirable {
     private double weight;
 
+    public Cheese(String name, int qunatity, double price, double weight) {
+        super(name, qunatity, price);
+        if(weight < 0)
+            throw new IllegalArgumentException("Weight cannot be negative");
+        this.weight = weight;
+    }
+
 
 }
