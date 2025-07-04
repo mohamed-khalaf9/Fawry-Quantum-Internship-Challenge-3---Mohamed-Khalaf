c69534191508005ff72b80620e8478f3c06b032c
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-04 23:11:15 +0300

    define mobile product class which is shippable

---
+++ src/main/java/Mobile.java
@@ -0,0 +1,26 @@
+
+public class Mobile extends  Product implements Shippable, ShippableItem {
+    private double weight;
+
+
+    public Mobile(String name, int qunatity, double price, double weight) {
+        super(name, qunatity, price);
+        if(weight < 0)
+            throw new IllegalArgumentException("Weight cannot be negative");
+
+        this.weight = weight;
+    }
+
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+
+
+    public String getName()
+    {
+        return super.getName();
+    }
+
+}
+
