8a560d4076f3d0eafa16c964bd8edac539f11e86
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-04 22:08:05 +0300

    define constructor, add validation on data members

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,7 +1,19 @@
 public abstract class Product {
     private String name;
-    private int qunatity;
+    private int quantity;
     private double price;
+
+    public Product(String name, int qunatity, double price) {
+        if(name == null || name.isEmpty())
+            throw new IllegalArgumentException("Product name cannot be empty");
+        if(qunatity < 0)
+            throw new IllegalArgumentException("qunatity must be a positive integer");
+        if(price < 0.0)
+            throw new IllegalArgumentException("price must be a positive number");
+        this.name = name;
+        this.quantity = qunatity;
+        this.price = price;
+    }
 
 
 
