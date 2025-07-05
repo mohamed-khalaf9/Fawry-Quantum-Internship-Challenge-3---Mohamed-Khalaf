4da6b09134a16ebed5c3e0708d37c276c0152968
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-05 04:28:27 +0300

    - define getShippableItems to return the shippable products with dublicates - define validateInputs for resuability if we added more inputs later - define validateNoExpiredProductsExist to organize the code - define validateCustomer Balance method - define method to calculate the subtotal separately - define method to calculate the shipping fees - define printCheckoutReceipt method - complete the logic of the checkout feature

--- src/main/java/CheckoutService.java
+++ src/main/java/CheckoutService.java
@@ -1,2 +1,104 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CheckoutService {
+    private static final double SHIPPING_FEES_PER_KG = 10.0;
+
+    public void checkout(Customer customer, Cart cart) {
+        validateInputs(customer, cart);
+
+        List<CartItem> cartItems = cart.getItems();
+        if(cartItems.isEmpty())
+            throw new IllegalStateException("Cart is empty.");
+
+        validateNoExpiredProductsExist(cartItems);
+
+        List<ShippableItem> shippableItems = getShippableItems(cartItems);
+        double subtotal = calculateSubtotal(cartItems);
+        double shippingFees = calculateShippingFees(cartItems);
+        double totalAmount = subtotal + shippingFees;
+
+        validateCustomerBalance(customer, totalAmount);
+        customer.withdraw(totalAmount);
+
+        ShippingService shippingService = new ShippingService();
+        shippingService.ship(shippableItems);
+
+        printCheckoutReceipt(cartItems, subtotal, shippingFees, totalAmount);
+    }
+
+    //::::::::::::::::::::::::::: Utilities :::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    public List<ShippableItem> getShippableItems(List<CartItem> cartItems) {
+        List<ShippableItem> shippableItems = new ArrayList<>();
+        for(CartItem cartItem : cartItems) {
+            if(cartItem.getProduct() instanceof ShippableItem shippableItem) {
+                for(int quantity = 0; quantity < cartItem.getQuantity(); quantity++) {
+                    shippableItems.add(shippableItem);
+                }
+            }
+        }
+        return shippableItems;
+    }
+
+    private void validateInputs(Customer customer, Cart cart) {
+        if (customer == null || cart == null) {
+            throw new IllegalArgumentException("Customer or cart cannot be null.");
+        }
+    }
+
+
+
+    private void validateNoExpiredProductsExist(List<CartItem> items) {
+        for (CartItem item : items) {
+            Product product = item.getProduct();
+            LocalDate currentDate = LocalDate.now();
+            if (product instanceof Expirable expirable && expirable.getExpiryDate().isBefore(currentDate)) {
+                throw new IllegalStateException(product.getName() + " is expired.");
+            }
+        }
+    }
+
+    private void validateCustomerBalance(Customer customer, double amount) {
+        if (customer.getCurrentBalance() < amount) {
+            throw new IllegalStateException("Insufficient balance to complete checkout.");
+        }
+    }
+
+
+
+    private double calculateSubtotal(List<CartItem> items) {
+        double subtotal = 0.0;
+        for (CartItem item : items) {
+            subtotal += item.getProduct().getPrice() * item.getQuantity();
+        }
+        return subtotal;
+    }
+
+    private double calculateShippingFees(List<CartItem> items) {
+        double shippingFees = 0.0;
+        for (CartItem item : items) {
+            Product product = item.getProduct();
+            if (product instanceof ShippableItem shippable) {
+                shippingFees += shippable.getWeight() * item.getQuantity() * SHIPPING_FEES_PER_KG;
+            }
+        }
+        return shippingFees;
+    }
+
+
+
+    private void printCheckoutReceipt(List<CartItem> items, double subtotal, double shippingFees, double total) {
+        System.out.println("** Checkout receipt **");
+        for (CartItem item : items) {
+            double itemTotal = item.getProduct().getPrice() * item.getQuantity();
+            System.out.printf("%dx %s %.0f%n", item.getQuantity(), item.getProduct().getName(),
+                    itemTotal);}
+
+        System.out.println("----------------------");
+        System.out.printf("Subtotal %.0f%n", subtotal);
+        System.out.printf("Shipping %.0f%n", shippingFees);
+        System.out.printf("Amount %.0f%n", total);
+    }
 }
