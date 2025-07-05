6a2bf6c4d1edcc2df50f516cd08a7a1a60f0b8b5
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-05 04:15:02 +0300

    - define ship method that prints the shipment notice and determine the quatnity using utility method that count the shippable items - define count shippable items method that count the quantity of each shippable item

--- src/main/java/ShippingService.java
+++ src/main/java/ShippingService.java
@@ -1,2 +1,40 @@
+import java.util.*;
+
 public class ShippingService {
+
+    public void ship(List<ShippableItem> items) {
+        System.out.println("** Shipment notice **");
+
+        if (items == null || items.isEmpty()) {
+            System.out.println("No shippable items found");
+            return;
+        }
+
+        Map<ShippableItem, Integer> itemCounts = countShippableItems(items);
+        double totalWeight = 0.0;
+
+
+
+        for (Map.Entry<ShippableItem, Integer> entry : itemCounts.entrySet()) {
+            ShippableItem item = entry.getKey();
+            int quantity = entry.getValue();
+            double weight = item.getWeight();
+            totalWeight += weight * quantity;
+
+            System.out.printf("%dx %s %.0fg%n", quantity, item.getName(), weight * quantity * 1000);
+        }
+
+        System.out.printf("Total package weight %.1fkg%n", totalWeight);
+    }
+
+    private  Map<ShippableItem, Integer> countShippableItems(List<ShippableItem> items) {
+        Map<ShippableItem, Integer> countMap = new LinkedHashMap<>();
+
+        for (ShippableItem item : items) {
+            countMap.put(item, countMap.getOrDefault(item, 0) + 1);
+        }
+
+        return countMap;
+    }
+
 }
