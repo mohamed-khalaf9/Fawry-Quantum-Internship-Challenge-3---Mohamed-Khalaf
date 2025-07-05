96398a6bb41096bb886fc40127708eb7a79a815e
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-07-05 04:53:15 +0300

    - test some usecase

--- src/main/java/EcommerceSystem.java
+++ src/main/java/EcommerceSystem.java
@@ -1,5 +1,22 @@
+import java.time.LocalDate;
+
 public class EcommerceSystem {
     public static  void main(String[] args) {
+        runUseCases();
 
+    }
+    public static void runUseCases() {
+        runFristUseCase();
+    }
+    public static void runFristUseCase() {
+        System.out.println("\n--- Expired Product Use Case ---");
+        try {
+            Biscuits expiredBiscuits = new Biscuits("Old Biscuits", 5, 10.0, LocalDate.now().minusDays(1));
+            Customer customer = new Customer(100.0, "Ahmed");
+            Cart cart = new Cart();
+            cart.add(expiredBiscuits, 1); // Should throw error
+        } catch (Exception e) {
+            System.out.println("Caught exception: " + e.getMessage());
+        }
     }
 }
